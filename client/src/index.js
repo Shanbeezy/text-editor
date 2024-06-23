fde4468d59f461ddca9b005819ec06ed40a2494c
Shannon James Jr. <jay.are.sj@gmail.com>
2024-06-23 19:01:13 -0400

    fixed import and changed root directory

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import { Workbox } from 'workbox-window';
-import Editor from './editor';
+import Editor from './js/editor';
 import './database';
 import '../css/style.css';
 
@@ -11,7 +11,7 @@ const loadSpinner = () => {
   spinner.classList.add('spinner');
   spinner.innerHTML = `
   <div class="loading-container">
-  <div class="loading-spinner" />
+  <div class="loading-spinner"></div>
   </div>
   `;
   main.appendChild(spinner);
@@ -27,7 +27,9 @@ if (typeof editor === 'undefined') {
 if ('serviceWorker' in navigator) {
   // register workbox service worker
   const workboxSW = new Workbox('/src-sw.js');
-  workboxSW.register();
+  workboxSW.register().catch(error => {
+    console.error('Service Worker registration failed:', error);
+  });
 } else {
-  console.error('Service workers are not supported in this browser.');
+  console.error('Service Worker registration is not supported in this browser.');
 }
